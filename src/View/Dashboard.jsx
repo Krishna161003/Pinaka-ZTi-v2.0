8184cd1a03c7e95b46d942f12b22b7e9f984b6ef
Krishna Raj <132978171+Krishna161003@users.noreply.github.com>
2025-08-15 13:06:03 +0530

    Update Dashboard.jsx

--- src/View/Dashboard.jsx
+++ src/View/Dashboard.jsx
@@ -604,7 +604,7 @@ const Dashboard = () => {
               style={{
                 marginTop: 10,
                 padding: 2,
-                height: "60px",
+                height: "58px",
                 background: colorBgContainer,
                 // borderRadius: borderRadiusLG,
                 marginLeft: "20px",
