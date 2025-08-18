88564d62ace29ef2821e37eab63feb3bb3817133
Krishna Raj <132978171+Krishna161003@users.noreply.github.com>
2025-08-18 15:09:39 +0530

    Update Inventory.jsx

--- src/View/Inventory.jsx
+++ src/View/Inventory.jsx
@@ -354,7 +354,7 @@ const Inventory = () => {
                         danger 
                         ghost
                         disabled={loading}
-                        style={{ marginLeft: 8 }}
+                        style={{ marginLeft: 8, marginTop: 5 }}
                       >
                         Shutdown All
                       </Button>
@@ -372,7 +372,7 @@ const Inventory = () => {
                         type="primary"
                         ghost
                         disabled={loading}
-                        style={{ marginLeft: 8 }}
+                        style={{ marginLeft: 8, marginTop: 5 }}
                       >
                         Reboot All
                       </Button>
