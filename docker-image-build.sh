99489ed6ccba934f91dd0a771f42ac12877c8f82
Krishna Raj <132978171+Krishna161003@users.noreply.github.com>
2025-08-27 09:41:05 +0530

    dockerfile

--- docker-image-build.sh
+++ docker-image-build.sh
@@ -4,8 +4,8 @@ set -x
 docker build -t pinaka-zti-v2.0-react-app:1.1 .
 docker build -t pinaka-zti-v2.0-node-backend:1.1 ./backend/db
 
-docker tag pinaka-zti-v2.0-react-app:1.1 localhost:4000/pinaka-zti-v2.0-react-app:1.1
-docker tag pinaka-zti-v2.0-node-backend:1.1 localhost:4000/pinaka-zti-v2.0-node-backend:1.1
+# docker tag pinaka-zti-v2.0-react-app:1.1 localhost:4000/pinaka-zti-v2.0-react-app:1.1
+# docker tag pinaka-zti-v2.0-node-backend:1.1 localhost:4000/pinaka-zti-v2.0-node-backend:1.1
 
 # docker push localhost:4000/pinaka-zti-v2.0-react-app:1.1
 # docker push localhost:4000/pinaka-zti-v2.0-node-backend:1.1
