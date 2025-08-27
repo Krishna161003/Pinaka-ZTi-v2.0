b3efd22ab9f312ece1cb82fe7fb124153d22b76d
Krishna Raj <132978171+Krishna161003@users.noreply.github.com>
2025-08-27 10:11:09 +0530

    ss

--- docker-image-build.sh
+++ docker-image-build.sh
@@ -10,7 +10,7 @@ docker build -t pinaka-zti-v2.0-node-backend:1.1 ./backend/db
 # docker push localhost:4000/pinaka-zti-v2.0-react-app:1.1
 # docker push localhost:4000/pinaka-zti-v2.0-node-backend:1.1
 
-docker pull quay.io/keycloak/keycloak:12.0.4
+docker pull quay.io/keycloak/keycloak:latest
 docker pull postgres:15
 docker pull mysql:8.0
 
