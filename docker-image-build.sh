0a1862edf16a9f613418e82493a3ad6bd746165b
Krishna Raj <132978171+Krishna161003@users.noreply.github.com>
2025-08-27 10:35:28 +0530

    update docker file

--- docker-image-build.sh
+++ docker-image-build.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 set -x
 
+sudo docker stop registry
+sudo docker rm registry
+sudo docker volume rm registry-data
+
+sudo docker run -d -p 4000:5000 --name registry registry:2
+
+
 docker build -t pinaka-zti-v2.0-react-app:1.1 .
 docker build -t pinaka-zti-v2.0-node-backend:1.1 ./backend/db
 
-# docker tag pinaka-zti-v2.0-react-app:1.1 localhost:4000/pinaka-zti-v2.0-react-app:1.1
-# docker tag pinaka-zti-v2.0-node-backend:1.1 localhost:4000/pinaka-zti-v2.0-node-backend:1.1
+docker tag pinaka-zti-v2.0-react-app:1.1 localhost:4000/pinaka-zti-v2.0-react-app:1.1
+docker tag pinaka-zti-v2.0-node-backend:1.1 localhost:4000/pinaka-zti-v2.0-node-backend:1.1
 
-# docker push localhost:4000/pinaka-zti-v2.0-react-app:1.1
-# docker push localhost:4000/pinaka-zti-v2.0-node-backend:1.1
+docker push localhost:4000/pinaka-zti-v2.0-react-app:1.1
+docker push localhost:4000/pinaka-zti-v2.0-node-backend:1.1
 
 docker pull quay.io/keycloak/keycloak:latest
 docker pull postgres:15
