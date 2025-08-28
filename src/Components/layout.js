d5d917bffff22f47bf44a3edc5705f9a3f97ba9c
Krishna Raj <132978171+Krishna161003@users.noreply.github.com>
2025-08-28 16:11:26 +0530

    Update layout.js

--- src/Components/layout.js
+++ src/Components/layout.js
@@ -358,7 +358,7 @@ const AppLayout = ({ children }) => {
               width: 64,
               height: 25,
               marginBottom: 5,
-              marginTop: 5,
+              marginTop: 3,
               background: "transparent",
               border: "none",
             }}
